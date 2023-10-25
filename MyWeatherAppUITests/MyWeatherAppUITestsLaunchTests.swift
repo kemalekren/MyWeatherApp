7ab0d72c36d27322afc2e8a4518d177cca04c00f
Esat Kemal Ekren <kemalekren@hotmail.com>
2023-10-25 22:06:16 +0200

    Initial Commit

---
+++ MyWeatherAppUITests/MyWeatherAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MyWeatherAppUITestsLaunchTests.swift
+//  MyWeatherAppUITests
+//
+//  Created by Esat Kemal Ekren on 25/10/2023.
+//
+
+import XCTest
+
+final class MyWeatherAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
