ad76f72099de7f92106441a82a28d8bd1f4d2f60
Esat Kemal Ekren <kemalekren@hotmail.com>
2023-10-26 16:08:07 +0200

    typealias helper added

---
+++ MyWeatherApp/Helpers/Typealias.swift
@@ -0,0 +1,14 @@
+//
+//  Typealias.swift
+//  MyWeatherApp
+//
+//  Created by Esat Kemal Ekren on 26/10/2023.
+//
+
+import Foundation
+
+public typealias VoidHandler = () -> Void
+public typealias BoolHandler = (Bool) -> Void
+public typealias StringHandler = (String) -> Void
+public typealias IntHandler = (Int) -> Void
+public typealias CustomHandler<T> = (T) -> Void
